cdb2bf64733fda04359a37c9095ff99828631500
Kelvin-Ben <kevoben14@gmail.com>
2023-03-18 19:43:46 +0300

    Fix MIT license and also put route inside routes tag

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Greeting from './components/Greeting';
 
 function App() {
   return (
     <BrowserRouter>
+    <Routes>
       <Route exact path="/" component={Greeting} />
+    </Routes>
     </BrowserRouter>
   );
 }
