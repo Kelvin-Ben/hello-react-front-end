2da0879a18d303c1919ea15fb82001e566bb5844
Kelvin-Ben <kevoben14@gmail.com>
2023-03-18 10:54:32 +0300

    Create an greetingActions to fetch the api

---
+++ src/actions/greetingActions.js
@@ -0,0 +1,9 @@
+import axios from 'axios';
+
+export const getRandomGreeting = () => async (dispatch) => {
+  const res = await axios.get('http://localhost:3000/api/v1/greetings');
+  dispatch({
+    type: 'GET_RANDOM_GREETING',
+    payload: res.data.message,
+  });
+};
