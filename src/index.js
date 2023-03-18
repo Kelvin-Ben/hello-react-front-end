4a4cbfb3a72e9381d7c1eacbb854aa833a6e6032
Kelvin-Ben <kevoben14@gmail.com>
2023-03-18 10:59:33 +0300

    Create route file to render the app to the page

--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import { createStore } from 'redux';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import greetingReducer from './reducers/greetingReducer';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
+const store = createStore(greetingReducer);
+
+ReactDOM.render(
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
+  </Provider>,
+  document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
